5ff052ae6a3406498749b0aba1829a10cd368ffa
ChristianPucci91 <pucci.christian91@gmail.com>
2020-10-27 17:18:03 +0100

    code cleaning

--- esercizio1/js/script.js
+++ esercizio1/js/script.js
@@ -19,31 +19,33 @@ document.getElementById('input_reverse').innerHTML = inputUserReverse;
 // per capire se la parola inserita dall'utente sarà palindroma o no.
 // potremmo usare un ciclo per riordinare la parola inserita dall'utente all'interno della funzione
 
-function inputUserReverse(inputUser) {
-  // creo una variabile di output vuota nella funzione per la parola scritta al contrario
-  reverseWord = "";
+// Inizio funzione
+ function inputUserReverse(inputUser) {
+   // creo una variabile di output vuota nella funzione per la parola scritta al contrario
+   reverseWord = "";
 
- // inizio ciclo for
+   // inizio ciclo for
 
- // dichiaro che la variabile i è uguale alla parola inserita dall'utente -1, e decremento ad ogni ciclo
- // la variabile contatore "i" così che "i" sia uguale all'ultima lettera di "inputUser" e ad
- // ogni ciclo il for scrive nella variabile "reverseWord" esattamente la parola al contrario
- for (var i = inputUser.length -1; i >=0;  i--) {
-        reverseWord+= inputUser[i];
+   // dichiaro che la variabile i è uguale alla parola inserita dall'utente -1, e decremento ad ogni ciclo
+   // la variabile contatore "i" così che "i" sia uguale all'ultima lettera di "inputUser" e ad
+   // ogni ciclo il for scrive nella variabile "reverseWord" esattamente la parola al contrario
+   for (var i = inputUser.length -1; i >=0;  i--) {
+     reverseWord+= inputUser[i];
+   }
+   // fine ciclo for
+
+   // dichiaro un return della variabile vuota inserita sopra, così avrà come valore la parola
+   // inserita dall'utente ma scritta al contrario
+   return reverseWord;
  }
- // fine ciclo for
-
- // dichiaro un return della variabile vuota inserita sopra, così avrà come valore la parola
- // inserita dall'utente ma scritta al contrario
- return reverseWord;
-}
 // fine funzione
 
 // inizio condizione if & else
-if (inputUser == inputUserReverse) {
-  document.getElementById('result').innerHTML = "La parola è palindroma";
-}else {
-  document.getElementById('result').innerHTML = "La parola non è palindroma";
-}
+  if (inputUser == inputUserReverse) {
+    document.getElementById('result').innerHTML = "La parola è palindroma";
+  }else {
+    document.getElementById('result').innerHTML = "La parola non è palindroma";
+  }
+// fine condizione if & else
 
 // <<<<<<<<<<<<<<<<<<<<< FINE ESERCIZIO 1 >>>>>>>>>>>>>>>>>>>>>>
